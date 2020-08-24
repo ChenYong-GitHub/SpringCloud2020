41cd662629018428967d0cf8b363bf8a47614fc1
shiwenz <13926108685@163.com>
2020-08-24 17:22:11 +0800

    nacos 练习

--- cloud-alibaba-nacos-config/src/main/java/com/alibaba_tencent/springcloud/controller/ConfigClientController.java
+++ cloud-alibaba-nacos-config/src/main/java/com/alibaba_tencent/springcloud/controller/ConfigClientController.java
@@ -1,7 +1,9 @@
 package com.alibaba_tencent.springcloud.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +18,23 @@ public class ConfigClientController
     @Value("${config.info}")
     private String configInfo;
 
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
     @GetMapping("/config/info")
     public String getConfigInfo() {
         return configInfo;
+    }
+
+    @GetMapping("config/info2")
+    public String getConfigInfo2() {
+        String name = getProperty("persion.name");
+        String age = getProperty("persion.age");
+        String birthday = getProperty("persion.birthday");
+        return name + " " + age + " " +  birthday;
+    }
+
+    private String getProperty(String key) {
+        return applicationContext.getEnvironment().getProperty(key);
     }
 }
